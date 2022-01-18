8e7dbbda64ac5f27ea98df7acd5ed869b924eccd
Fernando Dias Seixas <nando21.net@gmail.com>
2022-01-18 08:50:57 -0300

    Exceptions, Validations, Security

---
+++ src/main/java/com/devsuperior/dslearnbds/repositories/ReplayRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslearnbds.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dslearnbds.entities.Reply;
+
+public interface ReplayRepository extends JpaRepository<Reply, Long> {
+
+}
